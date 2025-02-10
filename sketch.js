4e62b601de9b9eebb68fa464d46419e27d2dd2a2
ShaizaanCizen <shaizaanprasla@ocadu.ca>
2025-02-10 00:33:27 -0500

    CizenVerse

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var x = 1;
+var y = 1;
+
+
+
+function setup() {
+    let canvas = createCanvas(800, 300);
+    canvas.parent("sketch-container"); // Attach canvas inside the div
+}
+
+
+
+
+function draw(){
+    background(100)
+    rect(x, y,80,60)
+    rect(100, 150,50,60)
+    rect(200, 50,80,40)
+    rect(300, 250,40,60)
+    rect(30, 200,50,30)
+    rect(400, 125,90,60)
+    rect(475, 0,100,30)
+    rect(450, 225,100,80)
+    rect(575, 100,30,50)
+    rect(650, 150,80,60)
+    rect(700, 10,100,70)
+
+}
+
+
